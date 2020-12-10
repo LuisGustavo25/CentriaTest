644b670e42bbf9b79bac6affd6e7a82fe53a3022
LuisGustavo <Acostarluisg@gmail.com>
2020-12-10 09:07:02 -0400

    init 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-
+import icon from './icon/pokeball.svg';
 import './App.css';
 import {useState,useEffect} from 'react';
+//1st Data from: https://pokeapi.co/api/v2/pokemon
 
-//Importando todos los elementos
-// <img src={logo} className="App-logo" alt="logo" />
-//import { datos } from './task.json'
-//https://pokeapi.co/api/v2/pokemon
-//Clase Johanistica:
-//1er: const [ elpo, setElpo ] = useState ("Johan es Un:"); Donde el primero es la variable y el segundo una función
-//2do: Primer item la Arrow Function, segundo el Array.
 
 function App() {
 
   const [ pokemons, setPokemons ] = useState ([]);
   useEffect(()=>{
-    fetch("https://pokeapi.co/api/v2/pokemon")
-    .then(data=>data.json())
+    fetch("https://pokeapi.co/api/v2/generation/1")
+    .then(data=>data.json() )
     .then(data=>setPokemons(data.results))
-    console.log(pokemons)
+    .catch(err =>{
+      console.log("There is An Error.")
+    })
     return ()=>{
     
     }
@@ -25,7 +21,22 @@ function App() {
 
   return (
     <div className="App">
-      <p>{pokemons.map((pokemon)=><p>{pokemon.name}</p>)}</p>
+      <div id="site-header" className="Header-module-firts">
+        <header className="Header-module navbar navbar-dark bg-dark"> 
+        <a className="Header-module-logo"> 
+        <img src={icon} className="App-logo" alt="Pokedex-Order"/>
+        </a>
+
+          <nav >
+            <a href="" className="Header-link">About</a>      
+            <a href="" className="Header-link">Contact</a>      
+            <a href="" className="Header-link">Documentatión</a>      
+        </nav>
+        </header>
+        
+      </div>
+
+      {/*<p>{pokemons.map((pokemon)=><p>{pokemon}</p>)}</p>*/}
       <header className="App-header">
        
       </header>
